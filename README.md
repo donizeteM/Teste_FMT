71b179f6e12c9ff39f6ad7b718bd5eb37c4e4575
Jose Donizete <josedonizeteem@gmail.com>
2023-11-14 20:50:07 -0300

    ajustes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # teste_FMT
 fazendo teste de linguagem html, css, javascript  
+
+
+
+*O projeto vai começar com HTML.
+*Proximo passo vai ser com javascript.
--- add.js
+++ add.js
@@ -1,7 +1,2 @@
 console.log("primeiro projeto em desenvolvimento");
-let contador = 0;
-while(contador <= 10){
-    console.log(contador);
-    contador = contador + 1;
-    contador += 1;
-}
+
